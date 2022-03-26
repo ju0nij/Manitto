9628f5eb4145abf62f5e58d90281234610acf4a7
Juyeong J <juyeong.j@outlook.com>
2022-03-27 02:34:14 +0900

    Update 1

--- random/MainWindow.xaml.cs
+++ random/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.IO;
+using System.Windows;
 
 namespace random
 {
@@ -39,8 +27,6 @@ namespace random
                     total[i, j] = 0;
                 }
             }
-            FileStream f = File.Create("result.txt");
-            f.Close();
             StreamWriter sw = new StreamWriter("result.txt");
             int c = 0;
             do
@@ -59,13 +45,16 @@ namespace random
                 c++;
 
             } while (c <= Class_Num);
+            StreamWriter swe;
             for (int p = 1; p < 2; p++)
             {
-
                 for (int q = 0; q < Class_Num; q++)
                 {
-                    log.Text += q+1 + "번의 마니또는 " + total[p, q] + "번 입니다.\n";
-                    sw.WriteLine( q + 1 + "번의 마니또는 " + total[p, q] + "번 입니다.");
+                    swe = new StreamWriter("number-" + (q + 1) + ".txt");
+                    log.Text += (q + 1) + "번의 마니또는 " + total[p, q] + "번 입니다.\n";
+                    sw.WriteLine((q + 1) + "번의 마니또는 " + total[p, q] + "번 입니다.");
+                    swe.WriteLine((q + 1) + "번의 마니또는 " + total[p, q] + "번 입니다.");
+                    swe.Close();
                 }
             }
             sw.Close();
